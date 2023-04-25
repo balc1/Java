7d7784d9e99bae4f04e76f6769d348b7a6b0e2a1
E.Balci <111460297+balc1@users.noreply.github.com>
2023-04-25 23:53:16 +0300

    Tek sayı girilene kadar 4 ün katı olan sayıları toplar

---
+++ TekSayıToplam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class TekSayıToplam {
+    public static void main(String[] args) {
+        int n;
+        int toplam = 0;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.print("Sayı giriniz :");
+            n = scan.nextInt();
+            if(n % 2 == 0 && n % 4 == 0){
+                toplam += n;
+            }
+        } while (n % 2 == 0);
+
+        System.out.println("Toplam :" + toplam);
+
+    }
+}
