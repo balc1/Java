4ea1172183d7eeeab450d64388245a558e3cc3ef
E.Balci <111460297+balc1@users.noreply.github.com>
2023-04-27 00:21:38 +0300

    Harmonik Toplama

---
+++ HarmonikTop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HarmonikTop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz :");
+        int n = input.nextInt();
+        double result=0;
+        for (int i=1;i<=n;i++){
+            result += (1.0/i);
+        }
+        System.out.println(result);
+
+
+
+    }
+}
