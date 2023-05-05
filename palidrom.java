19514e9fb2f498d10beb2cc9ab8dd01316faa5ec
E.Balci <111460297+balc1@users.noreply.github.com>
2023-05-05 14:41:07 +0300

    Palidrom number finder

---
+++ palidrom.java
@@ -0,0 +1,20 @@
+public class palidrom {
+
+    static boolean isPalidrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (reverseNumber == number)
+            return true;
+        else
+            return false;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalidrom(434));
+    }
+}
